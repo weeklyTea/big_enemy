18ea5eef0a17b69f419c22d199174ca5fbbd8e62
Anton Potapenko <anton.potapenko@ledas.com>
2020-02-08 14:57:46 +0700

    Added second player.

--- src/App.js
+++ src/App.js
@@ -2,41 +2,43 @@ import React, { useCallback, useEffect, useRef } from "react";
 import * as THREE from "three";
 import { Canvas, addEffect, useFrame } from "react-three-fiber";
 
-const preferences = {
-
-}
 const maxSpeed = 70 // Meters per second.
 const accel = 10
 const friction = 0.2 // Decrease in 'friction' times curSpeed each second.
 const rotateSpeed = THREE.Math.degToRad(70)
 const skiddingC = 0.8 // Should be dependent on player weight
 
+const shipLen = 39
+const shipWidth = 8
+
 const state = {
-  player: {
-    moveDir: new THREE.Vector3(0, 1, 0),
-    lookDir: new THREE.Vector3(0, 1, 0),
-    position: new THREE.Vector3(),
-    curSpeed: 0
+  players: {
+    1: {
+      moveDir: new THREE.Vector3(0, 1, 0),
+      lookDir: new THREE.Vector3(0, 1, 0),
+      position: new THREE.Vector3(0, 0, 0),
+      curSpeed: 0
+    }
   },
   keysPressed: {
-    left: false,
-    right: false,
-    up: false,
-    down: false
+    1: {
+      left: false,
+      right: false,
+      up: false,
+      down: false
+    },
   }
 };
 
-const shipLen = 40
-const shipWidth = 8
-
-function Ship() {
-  const ref = useRef();
+function Ship({ pId }) {
+  const ref = useRef()
 
   useFrame(() => {
     const mesh = ref.current
-    mesh.quaternion.setFromUnitVectors(new THREE.Vector3(1,0,0), state.player.lookDir.clone().normalize())
-    mesh.position.copy(state.player.position)
-  });
+    const { lookDir, position } = state.players[pId]
+    mesh.quaternion.setFromUnitVectors(new THREE.Vector3(1, 0, 0), lookDir.clone().normalize())
+    mesh.position.copy(position)
+  })
 
   return (
     <mesh ref={ref}>
@@ -46,57 +48,62 @@ function Ship() {
   );
 }
 
-let t1 = performance.now();
-function mainCycle(t2) {
-  const tDiff = (t2 - t1) / 1000; // Time diff in seconds
-  t1 = t2;
-  const { right, left, up, } = state.keysPressed;
+function movePlayer(pId, tDiff) {
+  const { right, left, up, } = state.keysPressed[pId];
 
-  let curSpeed = state.player.curSpeed;
+  let curSpeed = state.players[pId].curSpeed;
   if (right) {
-    state.player.lookDir.applyEuler(new THREE.Euler(0, 0, -rotateSpeed * tDiff))
+    state.players[pId].lookDir.applyEuler(new THREE.Euler(0, 0, -rotateSpeed * tDiff))
   }
   if (left) {
-    state.player.lookDir.applyEuler(new THREE.Euler(0, 0, rotateSpeed * tDiff))
+    state.players[pId].lookDir.applyEuler(new THREE.Euler(0, 0, rotateSpeed * tDiff))
   }
 
   if (up) {
     if (curSpeed + accel <= maxSpeed) {
-      state.player.curSpeed += accel * tDiff;
+      state.players[pId].curSpeed += accel * tDiff;
     }
 
-    const { lookDir, moveDir } = state.player
+    const { lookDir, moveDir } = state.players[pId]
     if (lookDir.angleTo(moveDir) < 0.02) {
-      state.player.moveDir.copy(lookDir)
+      state.players[pId].moveDir.copy(lookDir)
     } else {
-      state.player.moveDir.lerp(lookDir, skiddingC * tDiff)
+      state.players[pId].moveDir.lerp(lookDir, skiddingC * tDiff)
     }
   } else if (!up) {
-      const d = state.player.curSpeed * friction * tDiff
-      if (state.player.curSpeed - d < 0 || d < 0.01) state.player.curSpeed = 0
-      else state.player.curSpeed -= d
+    const d = state.players[pId].curSpeed * friction * tDiff
+    if (state.players[pId].curSpeed - d < 0 || d < 0.01) state.players[pId].curSpeed = 0
+    else state.players[pId].curSpeed -= d
   }
 
-  const { position, moveDir } = state.player
-  curSpeed = state.player.curSpeed
-  state.player.position.setX(position.x + curSpeed * tDiff * moveDir.x)
-  state.player.position.setY(position.y + curSpeed * tDiff * moveDir.y)
+  const { position, moveDir } = state.players[pId]
+  curSpeed = state.players[pId].curSpeed
+  state.players[pId].position.setX(position.x + curSpeed * tDiff * moveDir.x)
+  state.players[pId].position.setY(position.y + curSpeed * tDiff * moveDir.y)
+}
+
+let t1 = performance.now();
+function mainCycle(t2) {
+  const tDiff = (t2 - t1) / 1000; // Time diff in seconds
+  t1 = t2;
+
+  movePlayer(1, tDiff)
 }
 
 function App() {
   const keyDown = useCallback(e => {
     switch (e.keyCode) {
       case 39:
-        state.keysPressed.right = true;
+        state.keysPressed[1].right = true;
         return;
       case 37:
-        state.keysPressed.left = true;
+        state.keysPressed[1].left = true;
         return;
       case 38:
-        state.keysPressed.up = true;
+        state.keysPressed[1].up = true;
         return;
       case 40:
-        state.keysPressed.down = true;
+        state.keysPressed[1].down = true;
         return;
       default:
         return;
@@ -106,16 +113,16 @@ function App() {
   const keyUp = useCallback(e => {
     switch (e.keyCode) {
       case 39:
-        state.keysPressed.right = false;
+        state.keysPressed[1].right = false;
         return;
       case 37:
-        state.keysPressed.left = false;
+        state.keysPressed[1].left = false;
         return;
       case 38:
-        state.keysPressed.up = false;
+        state.keysPressed[1].up = false;
         return;
       case 40:
-        state.keysPressed.down = false;
+        state.keysPressed[1].down = false;
         return;
       default:
         return;
@@ -135,7 +142,7 @@ function App() {
     >
       <ambientLight intensity={0.1} />
       <pointLight position={[100, 100, 100]} intensity={2.2} />
-      <Ship />
+      <Ship pId={1}/>
     </Canvas>
   );
 }
